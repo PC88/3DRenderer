c092c721552345b7f3c6668bdea14995be760f9a
PC88 <32823980+PC88@users.noreply.github.com>
2019-09-04 17:10:48 +0100

    global func added, but commented for now, todo changed

--- 3DRenderer/Src/Renderer/Buffer.h
+++ 3DRenderer/Src/Renderer/Buffer.h
@@ -2,7 +2,32 @@
 #include <cstdint>
 #include <vector>
 
-// TODO: add conversion function gl types
+// TODO: move to file where types should be included
+//static GLenum ShaerDataTypeToOpenGLBaseType(ShaderDataType type)
+//{
+//	switch (type)
+//	{
+//	case ShaderDataType::None:   return GL_FLOAT;
+//	case ShaderDataType::Float:  return GL_FLOAT;
+//	case ShaderDataType::Float2: return GL_FLOAT;
+//	case ShaderDataType::Float3: return GL_FLOAT;
+//	case ShaderDataType::Float4: return GL_FLOAT;
+//	case ShaderDataType::Mat3:   return GL_FLOAT;
+//	case ShaderDataType::Mat4:   return GL_FLOAT;
+//	case ShaderDataType::Int:    return GL_INT;
+//	case ShaderDataType::Int2:   return GL_INT;
+//	case ShaderDataType::Int3:   return GL_INT;
+//	case ShaderDataType::Int4:   return GL_INT;
+//	case ShaderDataType::Bool:   return GL_BOOL;
+//	default:
+//		break;
+//	}
+//
+//	// error msg here for unknown shader data type
+//	return 0;
+//}
+
+
 
 enum class ShaderDataType
 {
